6a10ad44dfdee2848f6e40ee8c314d85c3b9f055
hiuchida <hiro4uchida@gmail.com>
2023-07-04 07:59:46 +0900

    Add InfoApiEx (apisoftlimitGet, symbolnameFutureGet, symbolnameOptionGet)

---
+++ src/main/java/com/github/hiuchida/kabusapi/client/ex/InfoApiEx.java
@@ -0,0 +1,42 @@
+package com.github.hiuchida.kabusapi.client.ex;
+
+import com.github.hiuchida.kabusapi.enums.symbolname.future.FutureCode;
+import com.github.hiuchida.kabusapi.enums.symbolname.option.PutOrCallCode;
+
+import io.swagger.client.ApiClient;
+import io.swagger.client.ApiException;
+import io.swagger.client.api.InfoApi;
+import io.swagger.client.model.ApiSoftLimitResponse;
+import io.swagger.client.model.SymbolNameSuccess;
+
+public class InfoApiEx {
+	private InfoApi api;
+
+	public InfoApiEx() {
+		api = new InfoApi();
+	}
+
+	public InfoApiEx(ApiClient apiClient) {
+		api = new InfoApi(apiClient);
+	}
+
+	public ApiSoftLimitResponse apisoftlimitGet(String X_API_KEY) throws ApiException {
+		ApiSoftLimitResponse response = api.apisoftlimitGet(X_API_KEY);
+		return response;
+	}
+
+	public SymbolNameSuccess symbolnameFutureGet(String X_API_KEY, Integer derivMonth, FutureCode futureCode)
+			throws ApiException {
+		String futureCodeStr = (futureCode != null) ? futureCode.toString() : null;
+		SymbolNameSuccess response = api.symbolnameFutureGet(X_API_KEY, derivMonth, futureCodeStr);
+		return response;
+	}
+
+	public SymbolNameSuccess symbolnameOptionGet(String X_API_KEY, Integer derivMonth, PutOrCallCode putOrCall, Integer strikePrice)
+			throws ApiException {
+		String putOrCallStr = (putOrCall != null) ? putOrCall.toString() : null;
+		SymbolNameSuccess response = api.symbolnameOptionGet(X_API_KEY, derivMonth, putOrCallStr, strikePrice);
+		return response;
+	}
+
+}
---
+++ src/test/java/com/github/hiuchida/kabusapi/client/ex/InfoApiExTest.java
@@ -0,0 +1,70 @@
+package com.github.hiuchida.kabusapi.client.ex;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+import com.github.hiuchida.kabusapi.enums.symbolname.future.FutureCode;
+import com.github.hiuchida.kabusapi.enums.symbolname.option.PutOrCallCode;
+
+import io.swagger.client.model.ApiSoftLimitResponse;
+import io.swagger.client.model.SymbolNameSuccess;
+
+/**
+ * API tests for InfoApiEx
+ */
+//@Ignore
+public class InfoApiExTest {
+
+    private final InfoApiEx api = new InfoApiEx();
+
+    /**
+     * ソフトリミット
+     *
+     * kabuステーションAPIのソフトリミット値を取得する
+     *
+     * @throws Exception
+     *          if the Api call fails
+     */
+    @Test
+    public void apisoftlimitGetTest() throws Exception {
+        String X_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        ApiSoftLimitResponse response = api.apisoftlimitGet(X_API_KEY);
+
+        // TODO: test validations
+    }
+    /**
+     * 先物銘柄コード取得
+     *
+     * 先物銘柄コード取得
+     *
+     * @throws Exception
+     *          if the Api call fails
+     */
+    @Test
+    public void symbolnameFutureGetTest() throws Exception {
+        String X_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        Integer derivMonth = 0;
+        FutureCode futureCode = FutureCode.日経225mini先物;
+        SymbolNameSuccess response = api.symbolnameFutureGet(X_API_KEY, derivMonth, futureCode);
+
+        // TODO: test validations
+    }
+    /**
+     * オプション銘柄コード取得
+     *
+     * オプション銘柄コード取得
+     *
+     * @throws Exception
+     *          if the Api call fails
+     */
+    @Test
+    public void symbolnameOptionGetTest() throws Exception {
+        String X_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        Integer derivMonth = 0;
+        PutOrCallCode putOrCall = PutOrCallCode.CALL;
+        Integer strikePrice = 0;
+        SymbolNameSuccess response = api.symbolnameOptionGet(X_API_KEY, derivMonth, putOrCall, strikePrice);
+
+        // TODO: test validations
+    }
+}
