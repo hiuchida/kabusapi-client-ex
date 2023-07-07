115004836018ccfffa70b99a1746c86394fa9c81
hiuchida <hiro4uchida@gmail.com>
2023-07-07 10:21:19 +0900

    Add BoardBeanUtil

---
+++ src/main/java/com/github/hiuchida/kabusapi/client_ex/pushapi/BoardBeanUtil.java
@@ -0,0 +1,26 @@
+package com.github.hiuchida.kabusapi.client_ex.pushapi;
+
+import com.google.gson.Gson;
+
+import io.swagger.client.JSON;
+
+/**
+ * 時価情報Beanクラスに関するユーティリティクラス。
+ */
+public class BoardBeanUtil {
+
+	/**
+	 * 時価情報BeanからJSON文字列を生成する。
+	 * 
+	 * @param bb 時価情報Bean。
+	 * @return JSON文字列。
+	 */
+	public static String toJsonString(BoardBean bb) {
+		JSON json = new JSON();
+		Gson gson = json.getGson();
+//		Gson gson = new Gson();
+		String s = gson.toJson(bb);
+		return s;
+	}
+
+}
